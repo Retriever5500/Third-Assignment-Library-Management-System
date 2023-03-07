fb518d41beb903d71b2bcf872966db11193fb0cb
Retriever5500 <Retriever5500@gmail.com>
2023-03-07 19:52:47 +0330

    Implement User class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,12 +1,44 @@
-public class User {
-    //User should have a list of books
-    //User should have a username and a password
+import java.util.List;
+import java.util.ArrayList;
 
-    public void rentBook(){
-        //TODO
+public class User {
+    private List<Book> books;
+
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
-    public void returnBook(){
-        //TODO
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return username;
+    }
+
+    // Checking if two users are identical
+    public boolean equals(User user) {
+        if(username == user.getUsername() && password == user.getPassword()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void rentBook(Library library, Book book){
+        // if there is at least one amount of the book, then we can lend it
+        if(library.isEnoughBook(book)) {
+            books.add(book);
+            library.decreaseBook(book, 1);
+        }
+    }
+
+    public void returnBook(Library library, Book book){
+        books.remove(book);
+        library.increaseBook(book, 1);
     }
 }
