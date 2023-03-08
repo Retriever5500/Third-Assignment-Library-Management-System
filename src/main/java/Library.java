54005ffbc5b625d538068ce4a2470cb78a2a0c62
Retriever5500 <Retriever5500@gmail.com>
2023-03-08 12:07:48 +0330

    Implement library class

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,84 +1,176 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Library {
-    /*
-    * The library should have a list of books.
-    * The library should have a map of books ISBNs which is linked to the amount of book
-    -> (for example: harry potter -> 4 means there are currently 4 harry potter books)
-    * The library should have a list of users and a list of librarians.
-     */
 
-    //book related functions
+    private List<Book> books;
+    private HashMap<Integer, Integer> booksAmount;
+    private List<Librarian> librarians;
+    private List<User> users;
 
-    public void addBook(){
-        //TODO
+    public Library() {
+        books = new ArrayList<Book>();
+        booksAmount = new HashMap<Integer, Integer>();
+        librarians = new ArrayList<Librarian>();
+        users = new ArrayList<User>();
     }
 
-    public void removeBook(){
-        //TODO
+    // book-related methods
+    public void addBook(Book book, int amount){
+        if(!doesBookExist(book)) {
+            books.add(book);
+            booksAmount.put(book.getISBN(), amount);
+        }
     }
 
-    public void searchBook(){
-        //TODO
+    public void removeBook(Book book){
+        if(doesBookExist(book)) {
+            books.remove(book);
+            booksAmount.remove(book.getISBN());
+        }
     }
 
-    public void updateBook(){
-        //TODO
+    public Book searchBook(String bookName){
+        for(Book candidateBook : books) {
+            if(candidateBook.getName() == bookName) {
+                return candidateBook;
+            }
+        }
+        return null;
     }
 
-    public void doesBookExist(){
-        //TODO
+    public void updateBook(Book outdatedBook, Book updatedBook) {
+        if(doesBookExist(outdatedBook)) {
+            books.remove(outdatedBook);
+            books.add(updatedBook);
+            int amount = booksAmount.get(outdatedBook.getISBN());
+            booksAmount.remove(outdatedBook.getISBN());
+            booksAmount.put(updatedBook.getISBN(), amount);
+        }
     }
 
-    public void increaseBook(){
-        //TODO
+    public boolean doesBookExist(Book book){
+        for(Book candidateBook : books) {
+            if(candidateBook.equals(book)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void decreaseBook(){
-        //TODO
+    // check if there are at least one copy of a given book
+    public boolean isEnoughBook(Book book) {
+        if(doesBookExist(book)) {
+            if(booksAmount.get(book.getISBN()) > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
-    //user related functions
-
-    public void addUser(){
-        //TODO
+    public void increaseBook(Book book, int amount){
+        if(doesBookExist(book)) {
+            int existedAmount = booksAmount.get(book.getISBN());
+            booksAmount.put(book.getISBN(), existedAmount+amount);
+        }
     }
 
-    public void removeUser(){
-        //TODO
+    public void decreaseBook(Book book, int amount){
+        if(doesBookExist(book)) {
+            int existedAmount = booksAmount.get(book.getISBN());
+            if(existedAmount >= amount) {
+                booksAmount.put(book.getISBN(), existedAmount-amount);
+            }
+        }
     }
 
-    public void searchUser(){
-        //TODO
+    // user-related functions
+
+    public void addUser(User user){
+        users.add(user);
     }
 
-    public void updateUser(){
-        //TODO
+    public void removeUser(User user){
+        users.remove(user);
     }
 
-    public void doesUserExist(){
-        //TODO
+    public User searchUser(String username){
+        for(User candidateUser : users) {
+            if(candidateUser.getUsername().equals(username)) {
+                return candidateUser;
+            }
+        }
+        return null;
     }
 
-    //librarian related functions
-
-    public void addLibrarian(){
-        //TODO
+    public void updateUser(User outdatedUser, User updatedUser){
+        if(doesUserExist(outdatedUser)) {
+            users.remove(outdatedUser);
+            users.add(updatedUser);
+        }
     }
 
-    public void removeLibrarian(){
-        //TODO
+    public boolean doesUserExist(User user){
+        for(User candidateUser : users) {
+            if(candidateUser.equals(user)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void searchLibrarian(){
-        //TODO
+    public boolean doesUsernameExist(String username) {
+        for(User candidateUser : users) {
+            String candidateUsername = candidateUser.getUsername();
+            if(username.equals(candidateUsername)) {
+                return true;
+            }
+        }
+        for(Librarian candidateLibrarian : librarians) {
+            String candidateUsername = candidateLibrarian.getUsername();
+            if(username.equals(candidateUsername)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void updateLibrarian(){
-        //TODO
+    //librarian-related functions
+
+    public void addLibrarian(Librarian librarian){
+        librarians.add(librarian);
     }
 
-    public void doesLibrarianExist(){
-        //TODO
+    public void removeLibrarian(Librarian librarian){
+        librarians.remove(librarian);
     }
 
+    public Librarian searchLibrarian(String username){
+        for(Librarian candidateLibrarian : librarians) {
+            if(candidateLibrarian.getUsername().equals(username)) {
+                return candidateLibrarian;
+            }
+        }
+        return null;
+    }
 
+    public void updateLibrarian(Librarian outdatedLibrarian, Librarian updatedLibrarian){
+        if(doesLibrarianExist(outdatedLibrarian)) {
+            librarians.remove(outdatedLibrarian);
+            librarians.add(updatedLibrarian);
+        }
+    }
+
+    public boolean doesLibrarianExist(Librarian librarian){
+        for(Librarian candidateLibrarian : librarians) {
+            if(candidateLibrarian.equals(librarian)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
